b6c18ee292e217af38234ac1f3fa7d77010bfe60
mcpehub <oqilovnurullohjon@gmail.com>
2026-05-05 07:06:56 +0500

    Delete CodeQuest-Platform directory

--- CodeQuest-Platform/src/js/firebase-config.example.js
+++
@@ -1,9 +0,0 @@
-// Copy this file to firebase-config.js and add your Firebase project config.
-window.CODEQUEST_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
-};
